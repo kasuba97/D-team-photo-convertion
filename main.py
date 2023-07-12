66c2ea8a3ad4e156ee3d8f9959a535e7ae6e5611
Geoffrey Kasuba Sichali <Kasubasich@gmail.com>
2023-07-12 12:09:04 +0200

    cut out renaming, factored the code, added color

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
 import cv2
+import termcolor as tc
+import os
+
+print('hey fam, please follow these steps:\n[1] copy the extracted folders of Female and Male in the input folder')
+todo = input(
+    '[?] type b: for both folders|| f: for only female|| m: for only male: ')
+start = input('[?] input y to start the convertion: ')
 
 
-print('hey artisee fam please follow these steps:\n1.. copy the extracted folders of Female and Male in the input folder\n2.. overwrite the already existing folders\n3.. rename all images to image (...) by pressing ctl + A, then F2 then type in image')
+def img_conv(command):
+    print('[✓] convention started please wait')
 
-start = input('4.. input y to start the convention: ')
+    def lady_work():
+        try:
+            tc.cprint('[✓] working on female folder...','yellow')
+            input_female = os.fspath('./input/Female/')
+            files = os.listdir(input_female)
+            for file in files:
+                og_img = cv2.imread(f'./input/Female/{file}', 1)
+                output = cv2.resize(og_img, (0, 0), fx=1024/416, fy=1024/416)
+                cv2.imwrite(f'./output/Female/{file}).jpeg', output)
+            return tc.cprint('[✓] convertion for female folder ended successfuly\n[-] find images in the output/Female','green')
+        except:
+            tc.cprint('[X] something went wrong please follow step 1','red')
+            ValueError
+            KeyError
+            TypeError
 
-if start=='y' or start == 'Y':
-    print('5.. convention started please wait\n6.. working...')
-    try:
-        for i in range(20):
-            og_img = cv2.imread(f'./input/Female/image ({i+1}).png',1)
-            output =  cv2.resize(og_img,(0,0),fx=1024/416,fy=1024/416)
-            cv2.imwrite(f'./output/Female/img({i+1}).jpeg',output)
-            og_img = cv2.imread(f'./input/Male/image ({i+1}).png',1)
-            output =  cv2.resize(og_img,(0,0),fx=1024/416,fy=1024/416)
-            cv2.imwrite(f'./output/Male/img({i+1}).jpeg',output)
-            
-    except:
-        ValueError
-        KeyError
-        print('hey artisee fam please follow these steps:\n1.. copy the extracted folders of Female and Male in the input folder\n2.. overwrite the already existing folders\n3.. rename all images to image (...) by pressing ctl + A, then F2 then type in image')
 
-    print('7.. convention ended successfully :)\n.... find the images in the output folder')
-else:
-    print('input y after following the steps fam')
+    def man_work():
+        try:
+            tc.cprint('[✓] working on male folder...','yellow')
+            input_male = os.fspath('./input/Male/')
+            files = os.listdir(input_male)
+            for file in files:
+                og_img = cv2.imread(f'./input/Male/{file}', 1)
+                output = cv2.resize(og_img, (0, 0), fx=1024/416, fy=1024/416)
+                cv2.imwrite(f'./output/Male/{file}', output)
+            return tc.cprint('[✓] convertion for male folder ended successfuly\n[-] find images in output/Male','green')
+        except:
+            tc.cprint('[X] something went wrong please follow step 1','red')
+            ValueError
+            KeyError
+            TypeError
+
+    if command == 'b' or command == 'B':
+        lady_work()
+        man_work()
+    elif command == 'f' or command == 'F':
+        lady_work()
+    elif command == 'm' or command == 'M':
+        man_work()
+    else:
+        return print('invalid command was passed')
+
+
+if start == 'y' or start == 'Y':
+    img_conv(todo)
